35c11f75c545704484a7dc4bb25b90f3e2a67454
suresh-pradeep <spradeep20@iitk.ac.in>
2022-03-12 22:24:27 +0530

    Added source files

---
+++ src/rows.js
@@ -0,0 +1,15 @@
+const rows = [
+  {
+    UniqueID: "1234",
+    Date: "08/03/2022",
+    DoctorName: "Dr. Gauranga ",
+    Prescribed_Physio: ["Traction"]
+  },
+  {
+    UniqueID: "1235",
+    Date: "01/03/2022",
+    DoctorName: "Dr. Krishna Yadav",
+    Prescribed_Physio: ["Traction", "Stretching"]
+  }
+];
+export default rows;
